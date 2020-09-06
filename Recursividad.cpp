7aa1d9e1ff2c14a1e57f120947e1ea3955f73366
Germán López Pineda <me1lopig@uco.es>
2020-09-06 18:38:41 +0200

    Agregar archivos de proyecto.

---
+++ Recursividad.cpp
@@ -0,0 +1,48 @@
+// Recursividad.cpp : Ejemplo de recurividad
+// suma de todos lso numeros positivos enteros hasta uno dado
+//
+
+
+
+long sumaEnteros(int n)
+{
+    // funcion de suma de todos los enteros hasta n
+    // en este caso lo hacemos por recursividad
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    else
+    {
+        return n + sumaEnteros(n - 1);
+    }
+
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+
+    cout << "Introduce el numero hasta donde hacer la suma:";
+    cin >> numero;
+    
+    cout << "La suma de todos los numeros enteros hasta " << numero << " es: " << sumaEnteros(numero) << endl;
+
+
+
+    cout << "\nPulsa una tecla para terminar " << endl;
+    cin.get();
+
+    return 0;
+
+}
+
+
