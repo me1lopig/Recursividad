c2fd7f5e8c974c890bacb6613658956e8f90e112
Germán López Pineda <33735247+me1lopig@users.noreply.github.com>
2020-09-19 20:31:04 +0200

    Ejemplo de Torres de Hanoi
    Ejercicio de torres de Hanoi con tres elementos

---
+++ torresDeHanoi/torresDeHanoi/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  torresDeHanoi
+//
+//  Created by German Lopez on 19/09/2020.
+//  Copyright © 2020 German Lopez. All rights reserved.
+//
+
+// ejercicio de las Torres de Hanoi
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    cout << "Hello, World!\n";
+    
+    
+    cout<<"Pulsa una tecla para terminar "<<endl;
+    cin.get();
+    return 0;
+}
