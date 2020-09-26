4a82c9a7002dec5360b314036eae572d4d1d1a1f
Germán López Pineda <me1lopig@uco.es>
2020-09-26 17:38:21 +0200

    20200921

---
+++ BusquedaBinaria/BusquedaBinaria.cpp
@@ -0,0 +1,20 @@
+// BusquedaBinaria.cpp 
+// busqueda binaria mediante recursividad
+//
+
+
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    cout << "Pulsa una tecla para terminar " << endl;
+    cin.get();
+
+    return 0;
+}
+
+
